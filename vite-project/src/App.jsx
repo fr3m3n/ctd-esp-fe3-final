b5e5184c87e76524127de8270af11baedca36051
fr3m3n <mtimochenko@tutanota.com>
2023-11-23 16:00:56 -0300

    template

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,68 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import Card from './Components/Card';
+
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [userError, setUserError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [showCard, setShowCard] = useState(false);
-  const [isFormValid, setIsFormValid] = useState(false);
-
-  // Check form validity on every input change
-  useEffect(() => {
-    const validateUser = username.length >= 3 && !username.startsWith(' ');
-    const validatePassword = password.length >= 6;
-    setIsFormValid(validateUser && validatePassword);
-
-    setUserError(validateUser ? '' : 'El usuario debe tener al menos 3 caracteres y no comenzar con un espacio.');
-    setPasswordError(validatePassword ? '' : 'La contraseña debe tener al menos 6 caracteres.');
-  }, [username, password]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (isFormValid) {
-      setShowCard(true);
-    }
-  };
-
-  // Add some styles to center the form
-  const formStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh' // This assumes the parent of this component has a defined height
-  };
-
   return (
-    <div style={formStyle}>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username"></label>
-        <input 
-          id="username"
-          type="text" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
-          placeholder="Usuario" 
-        />
-        {userError && <p>{userError}</p>}
-
-        <label htmlFor="password"></label>
-        <input 
-          id="password"
-          type="password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-          placeholder="Contraseña" 
-        />
-        {passwordError && <p>{passwordError}</p>}
-
-        <button type="submit" disabled={!isFormValid}>Submit</button>
-      </form>
-
-      {showCard && <Card username={username} password={password} />}
-    </div>
+      <div className="App">
+          <Navbar/>
+          <Footer/>
+      </div>
   );
 }
 
--- vite-project/src/Components/Card.jsx
+++ vite-project/src/Components/Card.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
 
-function Card({ username, password }) {
+
+const Card = ({ name, username, id }) => {
+
+  const addFav = ()=>{
+    // Aqui iria la logica para agregar la Card en el localStorage
+  }
+
   return (
-    <div>
-      <h3>Login</h3>
-      <p>User: {username}</p>
-      <p>Password: {password}</p>
+    <div className="card">
+        {/* En cada card deberan mostrar en name - username y el id */}
+
+        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+
+        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+        <button onClick={addFav} className="favButton">Add fav</button>
     </div>
   );
-}
+};
 
 export default Card;
