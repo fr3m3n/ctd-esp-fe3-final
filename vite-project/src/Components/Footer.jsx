38d6abd58a72e8e097d47caa185d59c839e2163a
fr3m3n <mtimochenko@tutanota.com>
2023-11-26 01:51:52 -0300

    [update] styles improved

--- vite-project/src/Components/Footer.jsx
+++ vite-project/src/Components/Footer.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from './utils/global.context'; // Adjust the import path as necessary
 
 const Footer = () => {
+  const { state } = useContext(GlobalContext);
+
+  // Define a variable to hold the path to your image
+  const logoPath = '../../images/DH.png'; // Make sure this path is correct
+
+  // Define a variable for dynamic styles based on the theme
+  const logoStyle = {
+    width: '200px', // Adjust the size as needed
+    filter: state.theme === 'dark' ? 'invert(100%)' : 'none', // Invert colors for dark mode
+    margin: '0 auto', // Center the logo
+    display: 'block' // Ensure the logo is a block element to center it correctly
+  };
+
   return (
     <footer>
-        <p>Powered by</p>
-        <img src="../../images/DH.png" alt='DH-logo' />
+      <p>Powered by</p>
+      <img src={logoPath} alt='DH logo' style={logoStyle} />
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
